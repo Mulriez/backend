7cf4d1113eb00e170202142c8680d1409c31eb7a
Mulriez <pesonarizqi17@gmail.com>
2023-01-11 14:38:37 +0700

    espress

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const http = require("http");
-const moment = require("moment/moment");
-const dayjs = require("dayjs");
-const { des, cekBilangan } = require("./example");
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/json");
-  //   res.write(des);
-  //   res.write(cekBilangan(7));
-  //   res.write(moment().calendar());
-  //   res.write(dayjs().month());
-  // res.write(
-  //     JSON.stringify({
-  //         status:"succsess",
-  //         message: "response success",
-  //         data : {
-  //             cekBilangan : cekBilangan(4),
-  //             des : des,
-  //             day : moment().calendar()
-  //         }
-  //     })
-  // )
-  const url = req.url;
-  if (url === "/sekolah") {
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "aku berangkat sekolah",
-      })
-    );
-  } else {
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "aku nggak sekolah",
-      })
-    );
-  }
-  res.end();
-});
+const express = require("express")
+const app = express()
+const port = 8080
 
-const hostname = "127.0.0.1";
-const port = 8085;
-server.listen(port, hostname, () => {
-  console.log(`Server Running at http://${hostname}:${port}/`);
+app.get("/", (req,res) => {
+  res.end("Hello World")
 });
+// app.get("/user", (req,res) => {
+//   res.send({
+//     status : "success",
+//     message : "Mulyeah"
+//   })
+// })
+app.get("/user", (req, res) => {
+  res.send({
+      status: "success",
+      message: "Running in get"
+  })
+})
+app.post("/user", (req, res) => {
+  res.send({
+      status: "success",
+      message: "Running in post"
+  })
+})
+app.put("/user", (req, res) => {
+  res.send({
+      status: "success",
+      message: "Running in put"
+  })
+})
+app.delete("/user", (req, res) => {
+  res.send({
+      status: "success",
+      message: "Running in delete"
+  })
+})
+app.get("/siswa/:nama", (req, res) => {
+  res.send({
+      status: "success",
+      message: `Atas nama ${req.params.nama}`
+  })
+})
+
+
+app.listen(port, () => console.log(`Running in http://localhost:${port}`))
